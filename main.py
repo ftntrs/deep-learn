434b908e75c8ce02b8f9c55904a50568e211c76f
Rui Shi <rshi@fortinet.com>
2025-02-05 14:33:21 -0800

    update keras

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-print('hello')
+import keras
+
+# from keras.models import Sequential
+# from keras.layers import Dense
+# from keras.utils import to_categorical
+#
+# model = Sequntial()
+# n_cols = car_data.shape[1]
+# target = to_categorical(target)
+
+import pandas as pd
+import numpy as np
+import keras
+
+import warnings
+warnings.simplefilter('ignore', FutureWarning)
